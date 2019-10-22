21d6ebe394e5582b227efbffeb0e127b42f2d452
iradukunda28celine <iradukunda.ireneceline28@gmail.com>
2019-10-22 16:55:37 +0200

    reverse string

---
+++ reverse.js
@@ -0,0 +1,12 @@
+
+let string = reverseString('');
+console.log(string);
+function reverseString(str) {
+    let revstr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        revstr = revstr + str[i];
+    }
+    return revstr;
+}
+
+console.log(reverseString('boooootee'));
